32f1ccaa9eaa6e1cb552c1f222be9c67125b9ff8
TobiasAndren <tobiasandren@hotmail.com>
2024-12-12 15:58:58 +0100

    made a starter boilerplate and added some variables

---
+++ contents/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Euclid Hotel</title>
+    <link rel="stylesheet" href="../assets/css/global.css">
+</head>
+
+<body>
+
+    <header class="header">
+        <h2>Logo</h2>
+        <nav>
+            <a href="">menu items</a>
+        </nav>
+    </header>
+
+    <section aria-label="hero">
+
+    </section>
--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 <?php
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/contents/header.php";
+
+?>
+
+<section aria-label="welcome-article">
+
+</section>
+
+<section aria-label="book-room">
+
+</section>
+
+<section aria-label="discount-offer">
+
+</section>
+
+
+<?php
+
+require __DIR__ . "/contents/footer.php";
+
+?>
