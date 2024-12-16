4788b29f1928b5e807f17a46d36f08b50d7b1f11
TobiasAndren <tobiasandren@hotmail.com>
2024-12-16 10:56:24 +0100

    added some css files and some contents to the welcome article

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
+
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/contents/header.php";
 
 ?>
 
 <section aria-label="welcome-article">
-
-</section>
-
-<section aria-label="book-room">
-
+    <article>
+        <h3>The Night Belongs To You!</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere aperiam nesciunt, magnam ullam incidunt, tempore exercitationem natus id repudiandae veritatis alias. Repellat quae harum sunt cumque? Amet recusandae harum laboriosam.</p>
+    </article>
 </section>
 
 <section aria-label="discount-offer">
+
+</section>
+
+<section aria-label="cta">
+
+</section>
+
+<section aria-label="features">
+
+</section>
+
+<section aria-label="sign-up">
 
 </section>
 
