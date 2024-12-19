6f314b7cb4fa711c7ac0b504dacf3996446209a2
TobiasAndren <tobiasandren@hotmail.com>
2024-12-19 15:08:54 +0100

    added a form and some styling to the booking page

--- book-room.php
+++ book-room.php
@@ -27,7 +27,24 @@ $calendar->useFullDayNames();
 </section>
 
 <section aria-label="book">
+    <form action="submit" method="post" class="bookingsContainer">
+        <label for="arrivalDate" class="labelText">What day do you wish to arrive?</label>
+        <input type="date" name="arrivalDate" min="2025-01-01" max="2025-01-31" class="input">
 
+        <label for="departureDate" class="labelText">What day do you wish to depart?</label>
+        <input type="date" name="departureDate" min="2025-01-01" max="2025-01-31" class="input">
+
+        <label for="firstName" class="labelText">First Name</label>
+        <input type="text" name="firstName" class="input" placeholder="Your first name..">
+
+        <label for="lastName" class="labelText">Last Name</label>
+        <input type="text" name="lastName" class="input" placeholder="Your last name..">
+
+        <label for="lastName" class="labelText">Transfer Code</label>
+        <input type="text" name="transferCode" class="input" placeholder="Your transfer code.." required>
+
+        <button class="bookButton">Book now</button>
+    </form>
 </section>
 
 
